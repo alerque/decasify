ef476bcd248af2f71eb06c289ec786a31528874b
Caleb Maclennan <caleb@alerque.com>
2023-07-05 21:43:47 +0300

    chore: Mock up how we might handle word by word evaluation

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,42 @@
 use std::{error, result};
-use unicode_titlecase::{StrTitleCase};
+use unicode_titlecase::StrTitleCase;
 
 #[cfg(feature = "cli")]
 pub mod cli;
 
 pub type Result<T> = result::Result<T, Box<dyn error::Error>>;
 
-pub fn to_titlecase (string: &String, locale: &String) -> String {
+pub fn to_titlecase(string: &String, locale: &String) -> String {
+    let words: Vec<&str> = string.split_whitespace().collect();
     match locale.as_str() {
-        "tr" => string.to_titlecase_tr_or_az_lower_rest(),
-        "tr_TR" => string.to_titlecase_tr_or_az_lower_rest(),
-        _ => string.to_titlecase_lower_rest(),
+        "tr" => to_titlecase_tr(words),
+        "tr_TR" => to_titlecase_tr(words),
+        _ => to_titlecase_en(words),
     }
+}
 
+pub fn to_titlecase_en(_words: Vec<&str>) -> String {
+    String::from("English")
+    // string.to_titlecase_lower_rest()
+}
+
+pub fn to_titlecase_tr(words: Vec<&str>) -> String {
+    let mut words = words.iter();
+    let mut output: Vec<String> = Vec::new();
+    let first = words.next().unwrap();
+    output.push(first.to_titlecase_tr_or_az_lower_rest());
+    for word in words {
+        match is_reserved_tr(word.to_string()) {
+            true => {
+                output.push(word.to_titlecase_tr_or_az_lower_rest());
+            }
+            false => output.push(word.to_string()),
+        }
+    }
+    output.join(" ")
+}
+
+pub fn is_reserved_tr(_word: String) -> bool {
+    false
+    // “ve, ile, ya, veya, yahut, ki, da, de” sözleri ile “mı, mi, mu, mü” soru
 }
