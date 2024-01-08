43f67dcba0f02fcb1ada4565caf0e02ed221a997
Caleb Maclennan <caleb@alerque.com>
2024-01-08 15:47:29 +0300

    style(tests): Fix coding style to make rustfmt happy

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,32 +1,54 @@
-use assert_cmd::Command;
-use predicates::prelude::*;
+#[cfg(feature = "cli")]
+use {assert_cmd::Command, predicates::prelude::*};
 
 #[cfg(feature = "cli")]
 #[test]
 fn main() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    cmd.args(["foo"]).assert().success().stdout("Foo\n").stderr("");
+    cmd.args(["foo"])
+        .assert()
+        .success()
+        .stdout("Foo\n")
+        .stderr("");
 }
 
 #[cfg(feature = "cli")]
 #[test]
 fn main_stdin() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    cmd.write_stdin("foo").assert().success().stdout("Foo\n").stderr("");
+    cmd.write_stdin("foo")
+        .assert()
+        .success()
+        .stdout("Foo\n")
+        .stderr("");
 }
 
 #[cfg(feature = "cli")]
 #[test]
 fn main_help() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    cmd.args(["--help"]).assert().success().stdout(predicate::str::contains("Usage: ")).stderr("");
+    cmd.args(["--help"])
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("Usage: "))
+        .stderr("");
 }
 
 #[cfg(feature = "cli")]
 #[test]
 fn main_lang() {
     let mut en_cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    en_cmd.args(["-l", "en", "ide"]).assert().success().stdout("Ide\n").stderr("");
+    en_cmd
+        .args(["-l", "en", "ide"])
+        .assert()
+        .success()
+        .stdout("Ide\n")
+        .stderr("");
     let mut tr_cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    tr_cmd.args(["-l", "tr", "ilk"]).assert().success().stdout("İlk\n").stderr("");
+    tr_cmd
+        .args(["-l", "tr", "ilk"])
+        .assert()
+        .success()
+        .stdout("İlk\n")
+        .stderr("");
 }
