db4a253697adb8ffffe9a4cefaf238dd73736b6f
Caleb Maclennan <caleb@alerque.com>
2024-01-08 15:08:14 +0300

    test(cli): Add test to make sure CLI flags are handled

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,4 +1,5 @@
 use assert_cmd::Command;
+use predicates::prelude::*;
 
 #[cfg(feature = "cli")]
 #[test]
@@ -12,4 +13,20 @@ fn main() {
 fn main_stdin() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
     cmd.write_stdin("foo").assert().success().stdout("Foo\n").stderr("");
+}
+
+#[cfg(feature = "cli")]
+#[test]
+fn main_help() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.args(["--help"]).assert().success().stdout(predicate::str::contains("Usage: ")).stderr("");
+}
+
+#[cfg(feature = "cli")]
+#[test]
+fn main_lang() {
+    let mut en_cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    en_cmd.args(["-l", "en", "ide"]).assert().success().stdout("Ide\n").stderr("");
+    let mut tr_cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    tr_cmd.args(["-l", "tr", "ilk"]).assert().success().stdout("İlk\n").stderr("");
 }
