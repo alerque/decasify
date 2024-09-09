fd9ab2e84a80fe80cdd07f3ae622f8bd1abc1986
Caleb Maclennan <caleb@alerque.com>
2024-09-10 00:31:32 +0300

    feat(nvim): Add a new command that runs decasify titlecasing on input range

--- plugin/decasify.lua
+++ plugin/decasify.lua
@@ -2,13 +2,15 @@ if vim.g.loaded_decasify then
    return
 end
 
-log.trace("Loading decasify")
+local decasify = require("decasify")
 
-vim.api.nvim_create_user_command("Decasify", function ()
-   print("Run decasify")
-end)
-
-
-print("Loaded decasify")
+vim.api.nvim_create_user_command("Decasify", function (args)
+   local first, last = args.line1, args.line2
+   local lines = vim.api.nvim_buf_get_lines(0, first - 1, last, true)
+   for i, line in ipairs(lines) do
+      lines[i] = decasify.titlecase(line)
+   end
+   vim.api.nvim_buf_set_lines(0, first - 1, last, true, lines)
+end, { range = true })
 
 vim.g.loaded_decasify = true
