08668d58fa243570577ad6b34927499f1c571fb2
Caleb Maclennan <caleb@alerque.com>
2024-09-10 02:33:39 +0300

    feat(nvim): Add global default case

--- plugin/decasify.lua
+++ plugin/decasify.lua
@@ -2,11 +2,15 @@ if vim.g.loaded_decasify then
    return
 end
 
+if not vim.g.decasify_case then
+   vim.g.decasify_case = "title"
+end
+
 local decasify = require("decasify")
 
 vim.api.nvim_create_user_command("Decasify", function (args)
    local first, last = args.line1, args.line2
-   local caser = args.fargs[1]:gsub("case$", "") .. "case"
+   local caser = (args.fargs[1] and args.fargs[1] or vim.g.decasify_case):gsub("case$", "") .. "case"
    if type(decasify[caser]) ~= "function" then
       vim.notify(("Decasify doesn't know what case '%s' is."):format(caser))
       return false
@@ -16,6 +20,6 @@ vim.api.nvim_create_user_command("Decasify", function (args)
       lines[i] = decasify[caser](line)
    end
    vim.api.nvim_buf_set_lines(0, first - 1, last, true, lines)
-end, { desc = "Pass lines to decasify for recasing prose", nargs = "+", range = true })
+end, { desc = "Pass lines to decasify for recasing prose", nargs = "*", range = true })
 
 vim.g.loaded_decasify = true
