2fc751c45660b46fe696b597340a59b116deedc0
Caleb Maclennan <caleb@alerque.com>
2024-09-10 02:21:28 +0300

    feat(nvim): Add a subcommand to access all casing options

--- plugin/decasify.lua
+++ plugin/decasify.lua
@@ -6,11 +6,16 @@ local decasify = require("decasify")
 
 vim.api.nvim_create_user_command("Decasify", function (args)
    local first, last = args.line1, args.line2
+   local caser = args.fargs[1]:gsub("case$", "") .. "case"
+   if type(decasify[caser]) ~= "function" then
+      vim.notify(("Decasify doesn't know what case '%s' is."):format(caser))
+      return false
+   end
    local lines = vim.api.nvim_buf_get_lines(0, first - 1, last, true)
    for i, line in ipairs(lines) do
-      lines[i] = decasify.titlecase(line)
+      lines[i] = decasify[caser](line)
    end
    vim.api.nvim_buf_set_lines(0, first - 1, last, true, lines)
-end, { range = true })
+end, { desc = "Pass lines to decasify for recasing prose", nargs = "+", range = true })
 
 vim.g.loaded_decasify = true
