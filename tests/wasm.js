83a773c1018c397f77b8de24115436d47f14e822
Caleb Maclennan <caleb@alerque.com>
2024-10-21 13:24:36 +0300

    test(wasm): Excercise new all-purpose case function

--- tests/wasm.js
+++ tests/wasm.js
@@ -1,7 +1,11 @@
 // SPDX-FileCopyrightText: © 2023 Caleb Maclennan <caleb@alerque.com>
 // SPDX-License-Identifier: LGPL-3.0-only
 
-import { titlecase, uppercase, lowercase, Locale, StyleGuide } from '../pkg';
+import { case, titlecase, uppercase, lowercase, Case, Locale, StyleGuide } from '../pkg';
+
+var input = "ILIK SU VE İTEN RÜZGARLAR"
+var output = case(input, Case.Title, Locale.TR)
+console.log(output)
 
 var input = "ILIK SU VE İTEN RÜZGARLAR"
 var output = titlecase(input, Locale.TR)
