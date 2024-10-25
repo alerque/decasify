b658d0c9ad8fc7e677cad3f6f8f6966c923f8888
Caleb Maclennan <caleb@alerque.com>
2024-10-25 11:10:02 +0300

    style(lua): Move module exports to top of file

--- src/lua.rs
+++ src/lua.rs
@@ -6,6 +6,35 @@ use mlua::prelude::*;
 
 pub use crate::types::{Case, Locale, Result, StyleGuide};
 
+#[mlua::lua_module]
+fn decasify(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    exports.set(
+        "case",
+        LuaFunction::wrap_raw::<_, (Chunk, Case, Locale, StyleGuide)>(to_case),
+    )?;
+    exports.set(
+        "titlecase",
+        LuaFunction::wrap_raw::<_, (Chunk, Locale, StyleGuide)>(to_titlecase),
+    )?;
+    exports.set(
+        "lowercase",
+        LuaFunction::wrap_raw::<_, (Chunk, Locale)>(to_lowercase),
+    )?;
+    exports.set(
+        "uppercase",
+        LuaFunction::wrap_raw::<_, (Chunk, Locale)>(to_uppercase),
+    )?;
+    exports.set(
+        "sentencecase",
+        LuaFunction::wrap_raw::<_, (Chunk, Locale)>(to_sentencecase),
+    )?;
+    let version = option_env!("VERGEN_GIT_DESCRIBE").unwrap_or_else(|| env!("CARGO_PKG_VERSION"));
+    let version = lua.create_string(version)?;
+    exports.set("version", version)?;
+    Ok(exports)
+}
+
 impl FromLua for Chunk {
     fn from_lua(value: LuaValue, _: &Lua) -> LuaResult<Self> {
         match value {
@@ -43,33 +72,4 @@ impl FromLua for StyleGuide {
             _ => unimplemented!(),
         }
     }
-}
-
-#[mlua::lua_module]
-fn decasify(lua: &Lua) -> LuaResult<LuaTable> {
-    let exports = lua.create_table()?;
-    exports.set(
-        "case",
-        LuaFunction::wrap_raw::<_, (Chunk, Case, Locale, StyleGuide)>(to_case),
-    )?;
-    exports.set(
-        "titlecase",
-        LuaFunction::wrap_raw::<_, (Chunk, Locale, StyleGuide)>(to_titlecase),
-    )?;
-    exports.set(
-        "lowercase",
-        LuaFunction::wrap_raw::<_, (Chunk, Locale)>(to_lowercase),
-    )?;
-    exports.set(
-        "uppercase",
-        LuaFunction::wrap_raw::<_, (Chunk, Locale)>(to_uppercase),
-    )?;
-    exports.set(
-        "sentencecase",
-        LuaFunction::wrap_raw::<_, (Chunk, Locale)>(to_sentencecase),
-    )?;
-    let version = option_env!("VERGEN_GIT_DESCRIBE").unwrap_or_else(|| env!("CARGO_PKG_VERSION"));
-    let version = lua.create_string(version)?;
-    exports.set("version", version)?;
-    Ok(exports)
 }
