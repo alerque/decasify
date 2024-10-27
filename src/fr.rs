951ee408aa563c281f8be2254d759036622c2c23
Caleb Maclennan <caleb@alerque.com>
2024-10-27 11:16:15 +0300

    chore(crate): Spin up space for a French implementation

---
+++ src/fr.rs
@@ -0,0 +1,20 @@
+// SPDX-FileCopyrightText: © 2023 Caleb Maclennan <caleb@alerque.com>
+// SPDX-License-Identifier: LGPL-3.0-only
+
+use crate::{Chunk, StyleGuide};
+
+pub fn titlecase(_chunk: Chunk, _style: StyleGuide) -> String {
+    todo!();
+}
+
+pub fn lowercase(_chunk: Chunk) -> String {
+    todo!();
+}
+
+pub fn uppercase(_chunk: Chunk) -> String {
+    todo!();
+}
+
+pub fn sentencecase(_chunk: Chunk) -> String {
+    todo!();
+}
